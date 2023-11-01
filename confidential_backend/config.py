ee266ffba799e49f16e04793e5ae1dd75ad1bd9c
Ivan Cvitkovic <icvitkovic@gmail.com>
2023-10-31 17:02:47 -0700

    Allow config of cookie secure attribute

--- confidential_backend/config.py
+++ confidential_backend/config.py
@@ -15,6 +15,7 @@ PREFERRED_URL_SCHEME = os.getenv("PREFERRED_URL_SCHEME", 'http')
 SESSION_TYPE = os.getenv("SESSION_TYPE", 'redis')
 SESSION_REDIS = redis.from_url(os.getenv("SESSION_REDIS", "redis://127.0.0.1:6379"))
 SESSION_COOKIE_SAMESITE = os.getenv("SESSION_COOKIE_SAMESITE", 'None')
+SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", 'true').lower() == 'true'
 
 REQUEST_CACHE_URL = os.environ.get('REQUEST_CACHE_URL', 'redis://localhost:6379/0')
 REQUEST_CACHE_EXPIRE = 24 * 60 * 60  # 24 hours
