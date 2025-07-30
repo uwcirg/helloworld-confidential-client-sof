03618e37b88aacdc36e45c6fa477e5119ade6dc9
Paul F Bugni <pbugni@uw.edu>
2025-07-30 16:22:18 -0700

    Specialized logger (or default to app logger if not configured) for all responses from upstream FHIR server.

---
+++ confidential_backend/fhirresourcelogger.py
@@ -0,0 +1,30 @@
+"""Specialized logger for capturing all FHIR resources from upstream servers"""
+from flask import current_app
+import logging
+from pythonjsonlogger.jsonlogger import JsonFormatter
+
+
+def configure_resource_logger(logger):
+    """Only if configured, write to named file.  Otherwise use app logger"""
+    filename = current_app.config["FHIR_RESOURCES_LOGFILE"]
+    if not filename:
+        return current_app.logger
+
+    handler = logging.FileHandler(filename)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(JsonFormatter(
+        "%(asctime)s %(name)s %(levelname)s %(message)s"))
+    logger.addHandler(handler)
+    logger.propagate = False
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+def get_fhir_resource_logger():
+    logger = logging.getLogger("FHIR_RESOURCES")
+
+    # Avoid duplicates from multiple calls
+    if not logger.handlers:
+        logger = configure_resource_logger(logger)
+
+    return logger
