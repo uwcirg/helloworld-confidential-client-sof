92f128678b64967f58bd8cf9dae683e358d31242
Paul Bugni <pbugni@u.washington.edu>
2025-12-16 16:24:11 -0800

    redis session uses `msgpack` as default serialization format.

--- confidential_backend/wrapped_session.py
+++ confidential_backend/wrapped_session.py
@@ -1,4 +1,4 @@
-import pickle
+import msgpack
 from flask import g, current_app, request, session
 
 
@@ -42,8 +42,8 @@ def get_redis_session_data(session_id):
     # why doesn't this use the flask default JSON serializer?
     # (probably because the session is designed to hold non JSON serializable objects, like datetime)
     try:
-        session_data = pickle.loads(encoded_session_data)
-    except pickle.UnpicklingError:
+        session_data = msgpack.loads(encoded_session_data)
+    except msgpack.exceptions.FormatError:
         current_app.logger.error(f'Unable to load session data for {session_id}')
         current_app.logger.error(f'failed to decode {encoded_session_data}')
         session_data = {}
